5f8a36e7821ff2373c62cb2ebb3bae3f92c3df49
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2021-05-07 21:25:31 +0200

    (feat) Finalisation semaine projet

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const url = "http://heticlive.herokuapp.com"
+export const url = process.env.NODE_ENV === "production" ? "http://heticlive.herokuapp.com" : "http://localhost:3001";
+
+export const nms_url = process.env.NODE_ENV === "production" ? "http://20.199.65.242:8888" : "http://localhost:8888"
+
+export const rtmp_url = process.env.NODE_ENV === "production" ? "http://20.199.65.242:1935" : "http://localhost:1935"
